158ea0ec2bedd771bd1b25dfd9de7dc510499928
crotten <wildrunts@gmail.com>
2014-08-01 14:27:10 -0400

    Cleaned up the code

--- js/main.js
+++ js/main.js
@@ -12,26 +12,7 @@ $(document).ready(function() {
 
   });
 
-$('.Album1').on('click', '.image', function(event) {
-
-  event.preventDefault();
-  $(this).closest('li').siblings().addClass('invisible');
-  $(this).addClass('currentImage').removeClass('image');
-  $(this).closest('li').siblings().find('a').removeClass('image');
-});
-
-$('.Album1').on('click', '.currentImage', function(event) {
-
-  event.preventDefault();
-  $(this).addClass('image');
-  $(this).closest('li').siblings().find('a').addClass('image');
-  $(this).removeClass('currentImage');
-  $(this).closest('li').siblings().removeClass('invisible');
-
-});
-
-
-  $('.Album2').on('click', '.image', function(event) {
+  $('.album').on('click', '.image', function(event) {
 
     event.preventDefault();
     $(this).closest('li').siblings().addClass('invisible');
@@ -39,7 +20,7 @@ $('.Album1').on('click', '.currentImage', function(event) {
     $(this).closest('li').siblings().find('a').removeClass('image');
   });
 
-  $('.Album2').on('click', '.currentImage', function(event) {
+  $('.album').on('click', '.currentImage', function(event) {
 
     event.preventDefault();
     $(this).addClass('image');
@@ -48,24 +29,5 @@ $('.Album1').on('click', '.currentImage', function(event) {
     $(this).closest('li').siblings().removeClass('invisible');
 
   });
-
-  $('.Album3').on('click', '.image', function(event) {
-
-    event.preventDefault();
-    $(this).closest('li').siblings().addClass('invisible');
-    $(this).addClass('currentImage').removeClass('image');
-    $(this).closest('li').siblings().find('a').removeClass('image');
-  });
-
-  $('.Album3').on('click', '.currentImage', function(event) {
-
-    event.preventDefault();
-    $(this).addClass('image');
-    $(this).closest('li').siblings().find('a').addClass('image');
-    $(this).removeClass('currentImage');
-    $(this).closest('li').siblings().removeClass('invisible');
-
-  });
-
 
 });
